26c3b090a4bd8230fd71582d22ccc576b6211755
Project-DDB <daberwaladhaval4444@gmail.com>
2019-08-26 21:37:31 +0530

    edit index

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bhavin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
